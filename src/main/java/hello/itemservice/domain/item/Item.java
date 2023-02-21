971848f117bcd2e94046cfd5ac450064f952479e
Newbiedevelope <rbtjr20313@gmail.com>
2023-02-21 15:31:53 +0900

    first commit

---
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,28 @@
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "가격과 수량의 곱이 10,000이 넘게 입력해주세요.")
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
